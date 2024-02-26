23b9264fc093eccda25dc42d2e599d8124b5a960
Deedee <deedeeowoeye@gmail.com>
2024-02-26 20:19:05 +0100

    initial commit

---
+++ Python/Week 1/user_input.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where are you located at? ")
+
+print("Hello " + name + ", you are " + age + " years old. You are currently living at " + location)
